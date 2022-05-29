c3d2b864c5d87403566d3ccd56cce3e1fc5cb917
theo923 <theotam912@gmail.com>
2022-05-29 14:41:27 +0100

    implement characteristics of less css

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
 // import React from "react";
 
 const App = () => {
-  return <div>Initialize</div>;
+  return (
+    <div>
+      <h1 className="primary-text">Initialize</h1>
+      <button className="primary-btn">Button</button>
+      <ul id="menu">
+        <li>
+          <a href="#">Link 1</a>
+        </li>
+        <li>
+          <a href="#">Link 2</a>
+        </li>
+        <li>
+          <a href="#">Link 3</a>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default App;
